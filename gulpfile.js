33dbae10a8efd525e82883c103e13666bfee7ebd
amine <amine@adress.com>
2020-04-11 20:58:03 +0100

    =remove unecessary files

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-
-const gulp=require('gulp');
-const {cleanup,fonts,images,html,sass_to_css,js,watch} = require('./gulp/tasks/watch');
-
-exports.cleanup=cleanup;
-exports.recharge=gulp.series(cleanup,fonts,images,html,sass_to_css,js);
-exports.default = watch;
-
